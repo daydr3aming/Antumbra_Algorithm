d8fe403812855c832b559173ed943d2ba8cfca54
RogerMTY <rogelioguzman73@gmail.com>
2023-09-20 05:39:35 -0600

    Agregado la segunda difusion distinta que no fuera negar la matriz, XOR hecho mas complicado

--- main.cpp
+++ main.cpp
@@ -1,17 +1,80 @@
 #include "keygenerations.h"
 
-// Operacion XOR, usa la llave y el bloque de texto para generar el texto cifrado.
+// Un XOR mas complicado con arimetica modular y trabaja sobre un bloque entero
 vector<uint8_t> XOR(const vector<uint8_t>& block, const vector<uint8_t>& key) {
     vector<uint8_t> result;
     size_t length = min(block.size(), key.size());
     result.reserve(length);
 
     for (size_t i = 0; i < length; ++i) {
-        result.push_back(block[i] ^ key[i]);
+        result.push_back((block[i] ^ key[i]) % 0x10000) ;
     }
 
     return result;
 }
+
+// Funcion auxiliar para permutar un byte
+
+uint8_t bitwisePermute(uint8_t inputByte, const std::vector<int>& permutationPattern) {
+    uint8_t result = 0;
+
+    for (int i = 0; i < permutationPattern.size(); ++i) {
+        int sourceBitPosition = permutationPattern[i];
+    
+        uint8_t sourceBit = (inputByte >> sourceBitPosition) & 0x01;
+        result |= (sourceBit << i);
+    }
+
+    return result;
+}
+
+// Funcion auxiliar para permutar un byte al reves
+
+uint8_t reverseBitwisePermute(uint8_t permutedByte, const std::vector<int>& inversePermutationPattern) {
+    uint8_t result = 0;
+
+    for (int i = 0; i < inversePermutationPattern.size(); ++i) {
+        int destinationBitPosition = inversePermutationPattern[i];
+
+        uint8_t sourceBit = (permutedByte >> i) & 0x01;
+        result |= (sourceBit << destinationBitPosition);
+    }
+
+    return result;
+}
+
+// Generamos una subllave a partir de la llave original
+
+std::vector<uint8_t> subKeyGen(const std::vector<uint8_t>& key) {
+    std::vector<uint8_t> subkey(key.size(), 0);
+
+    const int rotation = 3;
+
+    for (size_t i = 0; i < key.size(); ++i) {
+
+        subkey[i] = (key[i] << rotation) | (key[i] >> (8 - rotation));
+    }
+
+    return subkey;
+}
+
+// Revertimos la subllave para obtener la llave original
+
+std::vector<uint8_t> reverseSubKey(const std::vector<uint8_t>& subkey) {
+    std::vector<uint8_t> originalKey(subkey.size(), 0);
+
+
+    const int rotation = 3;
+
+    for (size_t i = 0; i < subkey.size(); ++i) {
+
+        originalKey[i] = (subkey[i] >> rotation) | (subkey[i] << (8 - rotation));
+    }
+
+    return originalKey;
+}
+
+
 
 // Funcion de encriptacion principal de Antumbra
 vector<uint8_t> encrypt(const vector<uint8_t>& key, const vector<uint8_t>& plaintext, int rounds) {
@@ -30,34 +93,41 @@ vector<uint8_t> encrypt(const vector<uint8_t>& key, const vector<uint8_t>& plain
             }
         }
 
-        // Paso 1: XOR (Primera operacion de confusion)
-        // Hacemos un XOR del bloque con la llave 
+        // Paso 1: XOR modular con la llave 
         vector<uint8_t> xoredBlock = XOR(block, key);
 
-        // Paso 2: Rotacion de bits (Primera operacion de difusion)
+        // Paso 2: Shift circular o rotacion (Primera operacion de difusion)
         const int rotation = 3;
         for (size_t i = 0; i < 16; ++i) {
             xoredBlock[i] = (xoredBlock[i] << rotation) | (xoredBlock[i] >> (8 - rotation));
         }
 
-        // Paso 3: Generacion de la matriz sombra
+        // Paso 3: Generamos la matriz sombra
         std::vector<uint8_t> keyBytes(key.begin(), key.end());
         std::hash<std::string> hasher;
         srand(static_cast<unsigned int>(hasher(std::string(keyBytes.begin(), keyBytes.end()))));
 
-        vector<uint8_t> shadowMatrix(16, 0);
-        for (int i = 0; i < 16; ++i) {
-            int randomValue = (rand() % 3) + 1; // Generamos un numero aleatorio en el rango de 1 a 3
-            shadowMatrix[i] = static_cast<uint8_t>(randomValue);
+        std::vector<int> shadowMatrix(8, 0);
+
+        // Paso 4: Permutacion de bits (Segunda operacion de difusion)
+
+        // Iniciamos la matriz sombra con numeros del 0 al 7
+        for (int i = 0; i < 8; ++i) {
+            shadowMatrix[i] = static_cast<int>(i);
+        }
+        // Se hace un shuffle de Fisher-Yates para permutar la matriz sombra
+        for (int i = 7; i > 0; --i) {
+            int j = rand() % (i + 1);
+            std::swap(shadowMatrix[i], shadowMatrix[j]);
+        }
+        // Permutamos el bloque con la matriz sombra, cambiando de posicion
+        for (size_t i = 0; i < 16; ++i) {
+            xoredBlock[i] = bitwisePermute(xoredBlock[i], shadowMatrix);
         }
 
-        // Paso 4: Negacion de la matriz sombra (Segunda operacion de difusion)
-        for (int i = 0; i < 16; ++i) {
-            shadowMatrix[i] = ~shadowMatrix[i];
-        }
+        // Paso 5: Hacemos XOR al bloque permutado (Segunda operacion de confusion)
 
-        // Paso 5: XOR del bloque con la matriz sombra (Segunda operacion de confusion)
-        xoredBlock = XOR(xoredBlock, shadowMatrix);
+        xoredBlock = XOR(xoredBlock, key);
 
         // Reemplazamos el texto original con todos los bloques cifrados
         for (size_t i = 0; i < 16; ++i) {
@@ -69,10 +139,9 @@ vector<uint8_t> encrypt(const vector<uint8_t>& key, const vector<uint8_t>& plain
     }
 
     if (rounds > 1) {
-        // Repetimos recursivamente el proceso de encriptacion segun el numero de rondas
-        return encrypt(key, encryptedText, rounds - 1);
+        // Creamos una nueva subllave y repetimos el proceso de encripcion
+        return encrypt(subKeyGen(key), encryptedText, rounds - 1);
     }
-
     return encryptedText;
 }
 
@@ -94,27 +163,39 @@ vector<uint8_t> decrypt(const vector<uint8_t>& key, const vector<uint8_t>& ciphe
             }
         }
 
-        // Ahora haremos las operaciones de encripcion en reversa para poder desencriptar
-        // Empezamos con:
 
-        // Paso 1: Generamos la matriz sombra 
+        // Revertimos el XOR con un mismo XOR sobre la matriz sombra negada y el bloque encriptado
+        vector<uint8_t> reversedBlock = XOR(block, key);
+
+
+        // Generamos la matriz sombra
         std::vector<uint8_t> keyBytes(key.begin(), key.end());
         std::hash<std::string> hasher;
         srand(static_cast<unsigned int>(hasher(std::string(keyBytes.begin(), keyBytes.end()))));
 
-        vector<uint8_t> shadowMatrix(16, 0);
-        for (int i = 0; i < 16; ++i) {
-            int randomValue = (rand() % 3) + 1; // Rango de 1 a 3
-            shadowMatrix[i] = static_cast<uint8_t>(randomValue);
+        std::vector<int> shadowMatrix(8, 0);
+
+        // Permutamos pero al revez ahora
+        for (int i = 0; i < 8; ++i) {
+            shadowMatrix[i] = static_cast<int>(i);
         }
 
-        // Hacemos la negacion y lo podemos dejar asi, no necesitamos revetir la negacion
-        for (int i = 0; i < 16; ++i) {
-            shadowMatrix[i] = ~shadowMatrix[i];
+        // Fisher-Yates 
+        for (int i = 7; i > 0; --i) {
+            int j = rand() % (i + 1);
+            std::swap(shadowMatrix[i], shadowMatrix[j]);
         }
 
-        // Revertimos el XOR con un mismo XOR sobre la matriz sombra negada y el bloque encriptado
-        vector<uint8_t> reversedBlock = XOR(block, shadowMatrix);
+        // Creamos el patron de permutacion inverso
+        std::vector<int> inversePermutationPattern(8);
+        for (int i = 0; i < 8; ++i) {
+            inversePermutationPattern[shadowMatrix[i]] = i;
+        }
+
+        // Regresar el bloque a su posicion original
+        for (size_t i = 0; i < 16; ++i) {
+            reversedBlock[i] = reverseBitwisePermute(reversedBlock[i], shadowMatrix);
+        }
 
         // Invertimos la rotacion de bits al lado contrario por el mismo numero de rotaciones
         const int rotation = 3;
@@ -135,8 +216,8 @@ vector<uint8_t> decrypt(const vector<uint8_t>& key, const vector<uint8_t>& ciphe
     }
 
     if (rounds > 1) {
-        // Repetimos el proceso de desencripcion las mismas veces que el de encripcion
-        return decrypt(key, decryptedText, rounds - 1); 
+        // Repetimos el proceso de desencripcion con la subllave revertida
+        return decrypt(reverseSubKey(key), decryptedText, rounds - 1); 
     }
 
     return decryptedText;
@@ -160,13 +241,15 @@ std::string vectorToString(const std::vector<uint8_t>& input) {
     return ss.str();
 }
 
-// Funcion auxiliar para imprimir un vector de bytes en hexadecimal
+// Funcion auxiliar para imprimir un vector de bytes
+
 void printVector(const std::vector<uint8_t>& input) {
     for (uint8_t byte : input) {
         std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
     }
     std::cout << std::dec << std::endl;
 }
+
 
 // Funcion auxiliar para convertir un vector de bytes a un string con codigo ASCII
 std::string vectorToAscii(const std::vector<uint8_t>& data) {
@@ -205,6 +288,7 @@ int main() {
     vector<uint8_t> resultantkey = keygen.keygen();
     vector<uint8_t> plaintext = stringToVector(text);
 
+
     vector<uint8_t> cyphertext = encrypt(resultantkey, plaintext, rounds);
     cout << "Cyphertext: " << endl;
     printVector(cyphertext);
@@ -212,10 +296,13 @@ int main() {
     ofstream encryptedoutput("encryptedoutput.txt");
     encryptedoutput << vectorToString(cyphertext);
 
-
+    // Solucion super ineficiente pero funciona para la ultima subllave
+    for(int i = 0; i < rounds - 1; i++){
+        resultantkey = subKeyGen(resultantkey);
+    }
+    
     vector<uint8_t> decryptedtexthex = decrypt(resultantkey, cyphertext, rounds);
     string decryptedtext = vectorToAscii(decryptedtexthex);
-    cout << decryptedtext << endl;
 
     ofstream output("output.txt");
     output << decryptedtext;
